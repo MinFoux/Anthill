cc8cf4012852c047a2e348ed2a1bf8aa63355d7f
Min <milo.monsivais3@gmail.com>
2026-09-11 21:37:34 -0600

    Add project files.

---
+++ Anthill.cpp
@@ -0,0 +1,15 @@
+//  Quick outline of goals for Anthill:
+//  Memory of the last directory location picked by the user.
+//  Awareness of the files (ants) the user opened in the most recent session.
+//  
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string homeDir = "D:", selectedAnt = "Input.ant";
+    cout << "Welcome to Anthill!\n";
+    cout << "Press l to display all Ants\n";
+    cout << "Press f to enter selected Ant\n";
+    return 0;
+}
